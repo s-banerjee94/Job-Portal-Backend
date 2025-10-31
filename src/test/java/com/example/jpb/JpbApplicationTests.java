7c4e410eee20b63434fa8ed101ac1253910ef643
s-banerjee94 <shondha.tara94@gmail.com>
2025-10-31 20:48:02 +0530

    Initialize Spring Boot project for Job Portal Backend with Maven configuration, application setup, and dependencies.

---
+++ src/test/java/com/example/jpb/JpbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
